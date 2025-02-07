4471a1d2e268b0f4870e7747ffaeecd1d445ae0e
Rathindra Nath Biswas <rathininfo7@gmail.com>
2025-02-07 13:35:24 +0600

    completed About Me section

--- src/Pages/Home/AboutMe/AboutMe.jsx
+++ src/Pages/Home/AboutMe/AboutMe.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function AboutMe() {
+const AboutMe = () => {
   return (
-    <div>
-      <h1>This is About Me</h1>
+    <div className="py-10">
+        <div> 
+            <h1 className="font-bold text-4xl text-gray-900 text-center py-5">About Me</h1>
+        </div>
+        <div className="flex justify-center items-center">
+        <p className="text-center text-gray-500 w-3/6">
+        I'm a web developer focused on building clean, efficient websites and applications that provide  great user experiences. 
+        My goal is to combine thoughtful design with robust functionality to create seamless,  engaging web experiences.
+      </p>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner/Banner'
+import AboutMe from './AboutMe/AboutMe'
 
 function Home() {
   return (
     <div>
       <Banner></Banner>
+      <AboutMe></AboutMe>
     </div>
   )
 }
